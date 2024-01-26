828af1b597ed992ce3e2b6f2ed1302fff34d21eb
InterfoGithub <johnpeterricaro@gmail.com>
2024-01-27 06:11:17 +0800

    Added loading whenever users.length === 0

--- app/page.tsx
+++ app/page.tsx
@@ -2,24 +2,37 @@
 import Image from "next/image";
 import styles from "./page.module.css";
 import Gallery from "./gallery";
+import { useEffect, useState } from "react";
 
-export default async function Home() {
-  let users: any = [];
+export default function Home() {
+  const [users, setUsers] = useState<any>([]);
 
-  await fetch("https://jsonplaceholder.typicode.com/users")
-    .then((response) => {
-      if (!response.ok) {
-        throw new Error(`HTTP error! status: ${response.status}`);
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(
+          "https://jsonplaceholder.typicode.com/users"
+        );
+        if (!response.ok) {
+          throw new Error(`HTTP error! status: ${response.status}`);
+        }
+        const data = await response.json();
+        setUsers(data);
+      } catch (error) {
+        console.error("There was a problem with the fetch operation:", error);
       }
-      return response.json();
-    })
-    .then((data) => {
-      users = data;
-      console.log("users:", users);
-    })
-    .catch((error) => {
-      console.error("There was a problem with the fetch operation:", error);
-    });
+    };
+
+    fetchData();
+  }, []);
+
+  if (users.length === 0) {
+    return (
+      <main className={styles.main}>
+        <div>loading...</div>
+      </main>
+    );
+  }
 
   return (
     <main className={styles.main}>
